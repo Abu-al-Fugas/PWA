83200353f1fd7c40bd452d6f0b6dbe858e5b8941
Abu-al-Fugas <etoparadi@gmail.com>
2026-07-09 10:22:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
 const CACHE_NAME = 'smt-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon.png'
+  'index.html',
+  'manifest.json',
+  'icon.png'
 ];
 
 self.addEventListener('install', event => {
